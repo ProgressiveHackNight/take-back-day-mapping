2fa5e40c434c8ac072ab7d2cf24f368ca58cccc1
Meka Seymour <mekaseymour@gmail.com>
2018-03-04 14:20:02 -0500

    Revert "[Google Maps React] Get Google Map working in React"
    This reverts commit 206046fb042ed13575e91f7fabec552acb4c1957.

--- app/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './stylesheets/index.css';
-import App from './javascripts/components/App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
--- app/src/javascripts/components/EventsList.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const EventsList = () => {
-  return <div class="events-list">Hello Events</div>;
-};
-
-export default EventsList;
