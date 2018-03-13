cba340785afc9ce015b8732bffcf527e85a11484
Chandra Galbiati <chandra.galbiati@gmail.com>
2018-03-13 14:07:45 -0400

    [Tests] Wrap Map in provider

--- src/components/__tests_/Map.spec.jsx
+++ src/components/__tests_/Map.spec.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
 import mockData from '../../mockData';
+import configureStore from '../../configureStore';
 import Map from '../Map';
 
 describe('Map', () => {
   it('renders without crashing', () => {
     const locations = mockData;
 
-    mount(<Map
-      locations={locations}
-      selectedMarker={locations[0].id}
-      onLocationSelect={() => undefined}
-    />);
+    mount(
+      <Provider store={configureStore()}>
+        <Map
+          locations={locations}
+          selectedMarker={locations[0].id}
+          onLocationSelect={() => undefined}
+        />
+      </Provider>
+    );
   });
 });
