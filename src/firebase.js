a000e273bc5f97d0b4fe6fa7cfece2a0c4d6124b
Chandra Galbiati <chandra.galbiati@gmail.com>
2018-03-29 22:30:50 -0400

    Use Env Keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 // src/firebase.js
 import firebase from 'firebase';
 const config = {
-  apiKey: 'AIzaSyAGqSVpUm81B-KYs-cyKYq8XnLTbZ8HHJw',
-  authDomain: 'takebackmap.firebaseapp.com',
-  databaseURL: 'https://takebackmap.firebaseio.com',
-  projectId: 'takebackmap',
-  storageBucket: 'takebackmap.appspot.com',
-  messagingSenderId: '257650412128',
+  apiKey: process.env.FIREBASE_API_KEY || 'AIzaSyAGqSVpUm81B-KYs-cyKYq8XnLTbZ8HHJw',
+  authDomain: process.env.FIREBASE_DOMAIN || 'takebackmap.firebaseapp.com',
+  databaseURL: process.env.FIREBASE_DB_URL || 'https://takebackmap.firebaseio.com',
+  projectId: process.env.FIREBASE_ID || 'takebackmap',
+  storageBucket: process.env.FIREBASE_BUCKET || 'takebackmap.appspot.com',
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID || '257650412128',
 };
 
 firebase.initializeApp(config);
 export default firebase;
-
