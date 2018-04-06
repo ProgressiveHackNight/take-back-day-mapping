b02abb39fc15d04a76c83afc9ca3c9c39ca3beb2
Meka Seymour <mekaseymour@gmail.com>
2018-04-05 21:17:51 -0400

    Update styles to fit better into microsite

--- src/styles/javascript/colors.js
+++ src/styles/javascript/colors.js
@@ -1,7 +1,7 @@
 // export const LIGHT_GRAY = '#b1b1b1';
-export const LIGHT_GRAY = '#dfe0df';
-export const MEDIUM_GRAY = '#c2c2c2';
-export const DARK_GRAY = '#4b4b4b';
-export const DARK_DARK_GRAY = '#222';
-export const BLACK = '#000000';
-export const CTA_GREEN = '#1cc532';
+export const LIGHT_GRAY = "#dfe0df";
+export const MEDIUM_GRAY = "#c2c2c2";
+export const DARK_GRAY = "#4b4b4b";
+export const DARK_DARK_GRAY = "#2b2b2b";
+export const BLACK = "#000000";
+export const CTA_GREEN = "#1cc532";
