59fdac4192e8bcae511f59547ac7b34646a0ab5c
Chandra Galbiati <chandra.galbiati@gmail.com>
2018-04-17 00:53:39 -0400

    Remove env variables from firebase code

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 // src/firebase.js
 import firebase from 'firebase';
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || 'AIzaSyAGqSVpUm81B-KYs-cyKYq8XnLTbZ8HHJw',
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN || 'takebackmap.firebaseapp.com',
-  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL || 'https://takebackmap.firebaseio.com',
-  projectId: process.env.REACT_APP_FIREBASE_ID || 'takebackmap',
-  storageBucket: process.env.REACT_APP_FIREBASE_BUCKET || 'takebackmap.appspot.com',
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID || '257650412128',
+  apiKey: 'AIzaSyAGqSVpUm81B-KYs-cyKYq8XnLTbZ8HHJw',
+  authDomain: 'takebackmap.firebaseapp.com',
+  databaseURL: 'https://takebackmap.firebaseio.com',
+  projectId: 'takebackmap',
+  storageBucket: 'takebackmap.appspot.com',
+  messagingSenderId: '257650412128',
 };
 
 firebase.initializeApp(config);
