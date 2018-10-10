95953a9d826a1708839b7ad96124acd1e848159a
Eric Steinborn <Eric.Steinborn@ag.ny.gov>
2018-10-10 11:35:25 -0400

    Fix typo

--- src/gateways/event.js
+++ src/gateways/event.js
@@ -1,6 +1,6 @@
 export const event = {
   title: 'REMINDER: National Prescription Drug Take Back Day',
-  description: 'The next National Prescription Drug Take Back Day will be on October 27th.\\n\\nPlease check our website: https://clearyourcabinet.com today to find the locations closest to you and make a plan.\\n\\nThank you for taking an incredibly important step to address the crisis in New York.\\n\\n--Clear Your Cabinet is an initiative of New York State Attorney General.',
+  description: 'The next National Prescription Drug Take Back Day will be on October 27th.\\n\\nPlease check our website: https://clearyourcabinet.com today to find the locations closest to you and make a plan.\\n\\nThank you for taking an incredibly important step to address the crisis in New York.\\n\\n--Clear Your Cabinet is an initiative of The New York State Attorney General.',
   startTime: '2018-10-01T00:00:00-04:00',
   endTime: '2018-10-01T23:59:00-04:00'
 };
